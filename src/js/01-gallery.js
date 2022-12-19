04ac04b8e3acb60da0da9ffccac2bbbb147ff619
OsokinNikita <110087045+OsokinNikita@users.noreply.github.com>
2022-12-19 15:56:13 +0200

    gogo

---
+++ src/js/01-gallery.js
@@ -0,0 +1,28 @@
+import { galleryItems } from "./gallery-items";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryBox = document.querySelector(".gallery");
+const galleryMarkap = createGaleryMarkup(galleryItems);
+
+galleryBox.insertAdjacentHTML("beforeend", galleryMarkap);
+
+function createGaleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      (image) => `
+    <a class="gallery__item" href="${image.original}">
+      <img class="gallery__image" src="${image.preview}" alt="${image.description}" 
+ />
+    </a>
+`
+    )
+    .join("");
+}
+
+const simpleLightboxOptions = {
+  captionsData: "alt",
+  captionDelay: 250,
+};
+
+let gallerySet = new SimpleLightbox(".gallery a", simpleLightboxOptions);
---
+++ src/js/02-video.js
@@ -0,0 +1,19 @@
+import Player from "@vimeo/player";
+const throttle = require("lodash.throttle");
+
+const iframe = document.querySelector("iframe");
+
+const player = new Player(iframe);
+
+player.on("timeupdate", throttle(onPlayWidtUpdate, 1000));
+
+
+function onPlayWidtUpdate(e) {
+  localStorage.setItem("videoplayer-current-time", JSON.stringify(e.seconds));
+}
+
+const saveTimeCode = localStorage.getItem("videoplayer-current-time");
+if(saveTimeCode) {
+  player.setCurrentTime(saveTimeCode);
+}
+
